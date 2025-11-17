0694de01e73f51cde18980a57d698fa5ec4e0c14
oskarix7 <oskar.vyborny@gmail.com>
2025-11-17 10:53:38 +0100

    changed the index in Picking BaseVariable

--- Posting/mainscript.py
+++ Posting/mainscript.py
@@ -238,7 +238,8 @@ def Update_Notion(WhichVariables, Keywords, Cathegory):
 def Pick_BaseVariable(Cathegory_Name):
     for variable in SERVER_ADS:
         if variable["Cathegory"] == Cathegory_Name:
-            BASEVARIABLE_NUMBER_Json = SERVER_ADS.index(variable)["Ads"].random.randint(0, len(variable["Ads"])-1)
+            index = SERVER_ADS.index(variable)
+            BASEVARIABLE_NUMBER_Json = SERVER_ADS[index]["Ads"].random.randint(0, len(variable["Ads"])-1)
             return BASEVARIABLE_NUMBER_Json
 def main():
     Cathegory_NAME, Cathegory_PLACE, AccountNumber = ServersPicker() # Picks the server and account
