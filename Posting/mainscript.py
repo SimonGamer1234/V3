8496d5eecbf1f46d5ea7649a679997bbc31f3ec9
oskarix7 <oskar.vyborny@gmail.com>
2025-12-30 16:35:34 +0100

    Fix AdNumber logic in Get_Data function to reset correctly

--- Posting/mainscript.py
+++ Posting/mainscript.py
@@ -77,7 +77,7 @@ def Get_Data(Cathegories_data, Tracker_data): # Chooses in which servers it will
         Plan = Accounts[Cathegory_PLACE]
         AccountNumber = Plan["AccountNumber"]
         AdNumber = Plan["AdNumber"]
-        if AdNumber == 3:
+        if AdNumber == 2:
             Tracker_data["Accounts"][Cathegory_PLACE]["AdNumber"] = 0
         else:
             Tracker_data["Accounts"][Cathegory_PLACE]["AdNumber"] = AdNumber + 1
