f57a6cbe77183b9c331219b7cfd892e97d84428f
oskarix7 <oskar.vyborny@gmail.com>
2025-12-30 18:02:25 +0100

    Remove debug print statements from Pick_BaseVariable function

--- Posting/mainscript.py
+++ Posting/mainscript.py
@@ -313,9 +313,7 @@ def Update_Notion(WhichVariables, Keywords, Cathegory):
     return status_codes
 
 def Pick_BaseVariable(Server_ads_data,Cathegory_Name):
-    print(Server_ads_data)
     for variable in Server_ads_data:
-            print(variable)
             Cathegory = variable["Cathegory"]
             if Cathegory_Name == Cathegory:
                 index = Server_ads_data.index(variable)     
