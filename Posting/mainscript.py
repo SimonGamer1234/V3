5f7192b6944f2d012061fae32722e2a8250fa93c
oskarix7 <oskar.vyborny@gmail.com>
2026-02-24 19:15:47 +0100

    fix: correct variable usage in post_message function for channel and guild info

--- Posting/mainscript.py
+++ Posting/mainscript.py
@@ -189,9 +189,9 @@ def post_message(cathegory_json, account_token, ad_json, cathegory_name, account
         if status_code != 200:
             errors_log.append({
                 "guild_name": guild_name,
-                "guild_id": guild_id["id"],
-                "channel_name": channel_name["name"],
-                "channel_id": channel_id["id"],
+                "guild_id": guild_id,
+                "channel_name": channel_name,
+                "channel_id": channel_id,
                 "status_code": status_code
                             })
         else: 
