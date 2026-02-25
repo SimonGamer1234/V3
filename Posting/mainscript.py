3d3c53315bfd6cd61fd55d359d12fcc1b4d08d56
Simons <121897649+SimonGamer1234@users.noreply.github.com>
2026-02-25 07:43:49 +0100

    Update mainscript.py

--- Posting/mainscript.py
+++ Posting/mainscript.py
@@ -396,7 +396,7 @@ def main():
         errors_log, succesful_posts = post_message(cathegory_json, account_token, ad_json, cathegory_name, account_index, False) # Posts the Ad
         report_message_system = report_system(cathegory_name, account_username, errors_log=errors_log, ad_json=ad_json) # Handles any posting
         report_message_system_customer = report_customer(ad_json, succesful_posts) # Sends a report to the customer
-        cathegories_data, report_message_notion_PATCH_posts = update_posts_left(cathegories_data, ad_index, cathegory_index, ad_keywords, base_vars_data, succesful_posts) # Edits the amount
+        cathegories_data, report_message_notion_PATCH_posts = update_posts_left(cathegories_data,cathegory_json,cathegory_index, ad_index,ad_keywords, base_vars_data, succesful_posts) # Edits the amount
         report_message_gist_GET_cath = update_gist(Cathegories_gist_ID,cathegories_data, "cathegories.json")
         teport_message_gist_GET_tracker = update_gist(Tracker_gist_ID, tracker_data_new, "tracker.json")
         print(f"{report_message_system, report_message_system_customer}\n {report_message_gist_GET_cath, teport_message_gist_GET_tracker}\n {report_message_notion_PATCH_posts,report_message_notion_PATCH_tracker}")
